3bbc80b591117af2a9ce1b08a2ee9f5a034bf232
nozwock <57829219+nozwock@users.noreply.github.com>
2024-10-25 20:46:18 +0530

    Initialize logger in noitad-cli

--- crates/noitad-cli/src/main.rs
+++ crates/noitad-cli/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use color_eyre::eyre::Result;
+use noitad_lib::{defines::APP_CONFIG_PATH, log::RotatingWriter};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+fn main() -> Result<()> {
+    let (non_blocking, _guard) = tracing_appender::non_blocking(RotatingWriter::new(
+        3,
+        APP_CONFIG_PATH.join("logs"),
+        "noitd.log",
+    )?);
+    tracing_subscriber::registry()
+        .with(fmt::layer().with_writer(non_blocking))
+        .with(EnvFilter::from_default_env())
+        .init();
+
     println!("Hello, world!");
+
+    Ok(())
 }
