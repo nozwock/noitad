b9f79604d9dbf82478b91e19d537808f8eb408b9
nozwock <57829219+nozwock@users.noreply.github.com>
2024-10-26 18:37:27 +0530

    Fix wrong path being used to save logs

--- crates/noitad-cli/src/main.rs
+++ crates/noitad-cli/src/main.rs
@@ -1,13 +1,13 @@
 mod cli;
 
 use color_eyre::eyre::Result;
-use noitad_lib::{defines::APP_CONFIG_PATH, log::RotatingWriter};
+use noitad_lib::{defines::APP_CONFIG_DIR, log::RotatingWriter};
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 fn main() -> Result<()> {
     let (non_blocking, _guard) = tracing_appender::non_blocking(RotatingWriter::new(
         3,
-        APP_CONFIG_PATH.join("logs"),
+        APP_CONFIG_DIR.join("logs"),
         "noitd.log",
     )?);
     tracing_subscriber::registry()
