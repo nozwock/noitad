54aa1ea1f8a07d72a2a95a3fcbb9114ec880e1f5
nozwock <57829219+nozwock@users.noreply.github.com>
2024-10-27 01:46:20 +0530

    Use slice for arr param in group_equal_by_key() instead of Vec

--- crates/noitad-cli/src/utils.rs
+++ crates/noitad-cli/src/utils.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-pub fn group_equal_by_key<T, U>(arr: &Vec<T>, key: impl Fn(&T) -> U) -> HashMap<U, Vec<&T>>
+pub fn group_equal_by_key<T, U>(arr: &[T], key: impl Fn(&T) -> U) -> HashMap<U, Vec<&T>>
 where
     U: std::hash::Hash + std::cmp::Eq,
 {
